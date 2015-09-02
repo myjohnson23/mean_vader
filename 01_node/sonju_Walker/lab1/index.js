89dbc10166c058fe927653da2f6e7593fd677638
Sonju Walker <sonjuwalker@gmail.com>
2015-09-02 14:02:32 -0500

    pull

--- 01_node/sonju_Walker/lab1/index.js
+++ 01_node/sonju_Walker/lab1/index.js
@@ -1,9 +1,10 @@
-  // library for http
   var http = require('http');
-  // library for filesystem
-  var fs = require('fs');
+  // library for http
 
-  var json = fs.readFileSync('./data.json');
+  var fs = require('fs');
+  // library for filesystem
+
+  var json = fs.readFileSync('./myfavs.json');
 
   http.createServer(function(request, response) {
     response.writeHead(200);
@@ -11,5 +12,4 @@
     response.end();
   }).listen(5000);
 
-  // console msg to let us know its running
-  console.log('api server is ready');
+  console.log('Api server is up and ready');
